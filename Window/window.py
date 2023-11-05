358adaac86902f4d8ac4ebd11a3fb0bcad568edc
sardor86 <sshavkatov900@gmail.com>
2023-11-05 10:26:05 +0500

    added main function

--- Window/window.py
+++ Window/window.py
@@ -1,34 +1,39 @@
 import tkinter as tk
+from tkinter import messagebox as mb
 
 
 class Window:
-    def __init__(self, command_a, command_b, command_c, command_d):
+    def __init__(self, command, question):
         self.root = tk.Tk()
         self.root.title('Опросник')
-        self.root.geometry('800x700')
+        self.root.geometry('1600x700')
 
         self.question = tk.Label(self.root)
-        self.question.grid(row=0)
+        self.question.grid(row=0, columnspan=2)
 
-        self.butt_A = tk.Button(self.root, text='A', command=command_a)
+        self.butt_A = tk.Button(self.root, text='A', command=lambda: command('A', question))
         self.butt_A.grid(row=1, column=0)
         self.answer_A = tk.Label(self.root)
         self.answer_A.grid(row=1, column=1)
 
-        self.butt_B = tk.Button(self.root, text='B', command=command_b)
+        self.butt_B = tk.Button(self.root, text='B', command=lambda: command('B', question))
         self.butt_B.grid(row=2, column=0)
         self.answer_B = tk.Label(self.root)
         self.answer_B.grid(row=2, column=1)
 
-        self.butt_C = tk.Button(self.root, text='C', command=command_c)
+        self.butt_C = tk.Button(self.root, text='C', command=lambda: command('C', question))
         self.butt_C.grid(row=3, column=0)
         self.answer_C = tk.Label(self.root)
         self.answer_C.grid(row=3, column=1)
 
-        self.butt_D = tk.Button(self.root, text='D', command=command_d)
+        self.butt_D = tk.Button(self.root, text='D', command=lambda: command('D', question))
         self.butt_D.grid(row=4, column=0)
         self.answer_D = tk.Label(self.root)
         self.answer_D.grid(row=4, column=1)
+
+    def results(self, result: int):
+        mb.showerror('Количество правильных ответов',
+                     f'Количество правильных ответов {result}')
 
     def draw(self):
         self.root.mainloop()
--- main.py
+++ main.py
@@ -1,1 +1,53 @@
 from Window import Window
+from config import path
+from XlsxReader import XlsxReader
+import random
+
+
+class Question:
+    def __init__(self, question_path):
+        self.correct = None
+        self.result = 0
+        self.row = 0
+
+        self.xlsx = XlsxReader(question_path)
+        self.window = Window(command, self)
+
+    def next(self):
+        if self.xlsx.row_size == self.row:
+            self.window.results(self.result)
+            exit()
+
+        self.row += 1
+        data = self.xlsx.read(self.row)
+
+        self.window.question.configure(text=data[0].value)
+        answer_id = list(range(1, 5))
+
+        random.shuffle(answer_id)
+
+        self.window.answer_A.configure(text=data[answer_id[0]].value)
+        self.window.answer_B.configure(text=data[answer_id[1]].value)
+        self.window.answer_C.configure(text=data[answer_id[2]].value)
+        self.window.answer_D.configure(text=data[answer_id[3]].value)
+
+        self.correct = ['A', 'B', 'C', 'D'][answer_id.index(1)]
+
+
+def command(choose: str, quest: Question):
+    if quest.correct == choose:
+        quest.result += 1
+        print('\u001b[32m Правильно')
+    else:
+        print('\u001b[31m Неправильно')
+    quest.next()
+
+
+def main():
+    question = Question(path / 'questions.xlsx')
+    question.next()
+    question.window.draw()
+
+
+if __name__ == '__main__':
+    main()
